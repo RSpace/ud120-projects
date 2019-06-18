8d175196788315c5565f39d5329ff1ae656e1edf
Casper Fabricius <me@casperfabricius.com>
2019-06-18 11:49:16 +0200

    Understand text mini project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -7,6 +7,7 @@ import sys
 
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 """
     Starter code to process the emails from Sara and Chris to extract
@@ -41,21 +42,28 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            repls = {"sara": "", "shackleton": "", "chris": "", "germani": ""}
+            text = reduce(lambda a, kv: a.replace(*kv), repls.iteritems(), text)
 
             ### append the text to word_data
+            word_data.append(text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -66,10 +74,14 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
+print("word_data[152]", word_data[152])
 
 
 
 ### in Part 4, do TfIdf vectorization here
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorized = vectorizer.fit_transform(word_data)
+print(len(vectorizer.get_feature_names()))
 
 
+print("word_data[34597]", word_data[34597])
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -8,12 +8,12 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -28,19 +28,23 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        repls = {"\r": " ", "\n": " "}
+        text_string = reduce(lambda a, kv: a.replace(*kv), repls.iteritems(), text_string)
 
-
-
+        stemmer = SnowballStemmer("english")
+        unstemmed_words = text_string.split(" ")
+        unstemmed_words = filter(lambda word: word.strip() != "", unstemmed_words)
+        stemmed_words = [stemmer.stem(word) for word in unstemmed_words]
+        words = string.join(stemmed_words, " ")
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +55,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
